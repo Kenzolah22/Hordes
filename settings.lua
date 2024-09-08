f07d4c597740e70716ce0a1d0c91e82bfa97e315
Kenzolah22 <hevock@gmail.com>
2024-09-08 14:27:52 +0800

    Add files via upload

---
+++ settings.lua
@@ -0,0 +1,35 @@
+local gui = require "gui"
+local settings = {
+    enabled = false,
+    elites_only = false,
+    pit_level = 1,
+    salvage = false,
+    path_angle = 10,
+    reset_time = 1,
+    selected_chest_type = nil,
+    failover_chest_type = nil,
+    greater_affix_count = 0,
+}
+
+function settings:update_settings()
+    settings.enabled = gui.elements.main_toggle:get()
+    settings.elites_only = gui.elements.elite_only_toggle:get()
+    settings.salvage = gui.elements.salvage_toggle:get() -- Change this line
+    settings.aggresive_movement = gui.elements.aggresive_movement_toggle:get() -- Finn's movement logic
+    settings.aggresive_movement = gui.elements.aggresive_movement_toggle:get() -- Finn's movement logic
+    settings.path_angle = gui.elements.path_angle_slider:get()
+    settings.selected_chest_type = gui.elements.chest_type_selector:get()
+    settings.failover_chest_type = gui.elements.failover_chest_type_selector:get()
+    settings.always_open_ga_chest = gui.elements.always_open_ga_chest:get()
+    settings.loot_mothers_gift = gui.elements.loot_mothers_gift:get()
+    settings.merry_go_round = gui.elements.merry_go_round:get()
+    settings.movement_spell_to_objective = gui.elements.movement_spell_to_objective:get()
+    settings.open_chest_delay = gui.elements.open_chest_delay:get()
+    settings.boss_kill_delay = gui.elements.boss_kill_delay:get()
+    settings.chest_move_attempts = gui.elements.chest_move_attempts:get()
+    settings.use_salvage_filter_toggle = gui.elements.use_salvage_filter_toggle:get()
+    settings.affix_salvage_count = gui.elements.affix_salvage_count:get()
+    settings.greater_affix_count = gui.elements.greater_affix_count:get()
+end
+
+return settings
